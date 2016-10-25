fa70b73f7fd21c4e292c7bc6ba7bb0cf0a475693
Nick Blaisdell <nick.blaisdell@gmail.com>
2016-10-25 14:53:20 -0400

    more css added, article list started

--- server.rb
+++ server.rb
@@ -1,10 +1,14 @@
 require 'sinatra'
 require 'pry'
-
+require 'csv'
 set :bind, '0.0.0.0'  # bind to all interfaces
 
 get '/articles' do
-  @articles = File.readlines('.csv')
+  CSV.foreach("articles.csv") do |row|
+    @article_title = row[0]
+    @article_url = row[1]
+    @article_description = row[2]
+  end
   erb :index
 end
 
@@ -12,12 +16,27 @@ get '/articles/new' do
   erb :new
 end
 
-post '/articles/new' do
-  article = params['article']
-
-  File.open('articles.csv', 'a') do |file|
-    file.puts(article)
+post '/articles' do
+  @error = {}
+  @title = params['title']
+  if @title.empty?
+    @error[:title] = "You must include a Title"
+  end
+  @url = params['url']
+  if @url.empty?
+    @error[:url] = "You must include a URL"
+  end
+  @description = params['description']
+  if @description.empty?
+    @error[:description] = "You must include a Description"
   end
 
-  redirect '/articles'
+  if @error.keys.empty?
+    CSV.open('articles.csv', 'a') do |csv|
+      csv << [@title, @url, @description]
+    end
+    redirect '/articles'
+  else
+    erb :new
+  end
 end
